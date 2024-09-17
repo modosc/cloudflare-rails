f43de943e45494d6baf40e5066290866fe2620b9
Tony Novak <afn2@cornell.edu>
2024-09-17 12:42:25 -0400

    Add cloudflare? method to determine if request passed through CF (#149)
    * Add cloudflare? method to determine if request passed through CF
    
    The `request.cloudflare?` method can be used in a Rack::Attack blocklist rule
    to block traffic that hasn't passed through CloudFlare.
    
    For instance:
    
    ```ruby
      Rack::Attack.blocklist('CloudFlare WAF bypass') do |req|
        !req.cloudflare?
      end
    ```
    
    Note that the request may optionally pass through additional trusted
    proxies, so it will return true for any of these scenarios:
    
    * `REMOTE_ADDR` = CloudFlare
    * `REMOTE_ADDR` = *trusted_proxy*, `X_HTTP_FORWARDED_FOR` = CloudFlare,...
    * `REMOTE_ADDR` = *trusted_proxy*, `X_HTTP_FORWARDED_FOR` = *trusted_proxy2*,CloudFlare,...
    
    but it will return false if CloudFlare comes after the trusted prefix of
    `X-Forwarded-For`.
    
    * Address Rubocop warnings
    
    * Break up tests to only have one expectation per spec
    * Reduce line lengths

--- spec/cloudflare/rails_spec.rb
+++ spec/cloudflare/rails_spec.rb
@@ -103,6 +103,50 @@ describe CloudflareRails do
         end
       end
 
+      describe 'Rack::Request' do
+        before do
+          rails_app.initialize!
+        end
+
+        describe '#cloudflare?' do
+          it 'returns true if the request originated from CloudFlare directly' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '197.234.240.1')).to be_cloudflare
+          end
+
+          it 'returns true if the request originated from CloudFlare via one trusted proxy' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '10.1.1.1', 'HTTP_X_FORWARDED_FOR' => '197.234.240.1')).to be_cloudflare
+          end
+
+          it 'returns true if the request originated from CloudFlare via two trusted proxies' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '10.1.1.1',
+                                     'HTTP_X_FORWARDED_FOR' => '10.2.2.2,197.234.240.1')).to be_cloudflare
+          end
+
+          it 'returns true if the request originated from CloudFlare via one trusted proxy and one untrusted upstream IP' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '10.1.1.1',
+                                     'HTTP_X_FORWARDED_FOR' => '197.234.240.1,1.2.3.4')).to be_cloudflare
+          end
+
+          it 'returns false if the request did not originate from CloudFlare' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '1.2.3.4')).not_to be_cloudflare
+          end
+
+          it 'returns false if the request originated from CloudFlare via an untrusted REMOTE_ADDR' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '1.2.3.4',
+                                     'HTTP_X_FORWARDED_FOR' => '197.234.240.1')).not_to be_cloudflare
+          end
+
+          it 'returns false if the request has a trusted REMOTE_ADDR but did not originate from CloudFlare' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '10.1.1.1', 'HTTP_X_FORWARDED_FOR' => '1.2.3.4')).not_to be_cloudflare
+          end
+
+          it 'returns false if the request has a trusted REMOTE_ADDR and an untrusted proxy before CloudFlare' do
+            expect(Rack::Request.new('REMOTE_ADDR' => '10.1.1.1',
+                                     'HTTP_X_FORWARDED_FOR' => '1.2.3.4,197.234.240.1')).not_to be_cloudflare
+          end
+        end
+      end
+
       # functional tests - maybe duplicate of the remote_ip/ip tests above?
       describe 'middleware', type: :request do
         let(:base_ip) { '1.2.3.4' }
